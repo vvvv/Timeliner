13e73b1321844a7addd884bb4aac6645e1ea9842
joreg <joreg@vvvv.org>
2014-03-29 04:49:32 +0000

    cosmetics

--- TimelinerSA/Form1.Designer.cs
+++ TimelinerSA/Form1.Designer.cs
@@ -31,58 +31,48 @@
         /// </summary>
         private void InitializeComponent()
         {
-        	this.components = new System.ComponentModel.Container();
-        	this.textBox1 = new System.Windows.Forms.TextBox();
-        	this.panel1 = new System.Windows.Forms.Panel();
-        	this.webBrowser1 = new System.Windows.Forms.WebBrowser();
-        	this.timer1 = new System.Windows.Forms.Timer(this.components);
-        	this.panel1.SuspendLayout();
-        	this.SuspendLayout();
-        	// 
-        	// textBox1
-        	// 
-        	this.textBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
-        	this.textBox1.Location = new System.Drawing.Point(0, 475);
-        	this.textBox1.Multiline = true;
-        	this.textBox1.Name = "textBox1";
-        	this.textBox1.Size = new System.Drawing.Size(1009, 124);
-        	this.textBox1.TabIndex = 2;
-        	// 
-        	// panel1
-        	// 
-        	this.panel1.Controls.Add(this.webBrowser1);
-        	this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
-        	this.panel1.Location = new System.Drawing.Point(0, 0);
-        	this.panel1.Name = "panel1";
-        	this.panel1.Size = new System.Drawing.Size(1009, 475);
-        	this.panel1.TabIndex = 3;
-        	// 
-        	// webBrowser1
-        	// 
-        	this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
-        	this.webBrowser1.Location = new System.Drawing.Point(0, 0);
-        	this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
-        	this.webBrowser1.Name = "webBrowser1";
-        	this.webBrowser1.ScriptErrorsSuppressed = true;
-        	this.webBrowser1.Size = new System.Drawing.Size(1009, 475);
-        	this.webBrowser1.TabIndex = 0;
-        	// 
-        	// timer1
-        	// 
-        	this.timer1.Interval = 1;
-        	// 
-        	// Form1
-        	// 
-        	this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-        	this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-        	this.ClientSize = new System.Drawing.Size(1009, 599);
-        	this.Controls.Add(this.panel1);
-        	this.Controls.Add(this.textBox1);
-        	this.Name = "Form1";
-        	this.Text = "Form1";
-        	this.panel1.ResumeLayout(false);
-        	this.ResumeLayout(false);
-        	this.PerformLayout();
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.webBrowser1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1009, 599);
+            this.panel1.TabIndex = 3;
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.ScriptErrorsSuppressed = true;
+            this.webBrowser1.Size = new System.Drawing.Size(1009, 599);
+            this.webBrowser1.TabIndex = 0;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1009, 599);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "TimelinerSA";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
         }
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.WebBrowser webBrowser1;
@@ -90,7 +80,6 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
