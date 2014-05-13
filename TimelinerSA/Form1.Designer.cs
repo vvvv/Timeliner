4bc4f7670b171bfaccba1ba37f2e7788e3bdccbe
joreg <joreg@vvvv.org>
2014-05-14 01:36:27 +0200

    introduced OSC sending/remoting

--- TimelinerSA/Form1.Designer.cs
+++ TimelinerSA/Form1.Designer.cs
@@ -18,9 +18,6 @@
                 components.Dispose();
             }
             base.Dispose(disposing);
-            
-            foreach (var t in FPoshTimeliners)
-            	t.Dispose();
         }
 
         #region Windows Form Designer generated code
@@ -35,45 +32,184 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.panel1 = new System.Windows.Forms.Panel();
             this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.PrefixTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.TargetPortNumberBox = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TargetIPTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.ReceivePortNumberBox = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.TargetPortNumberBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ReceivePortNumberBox)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.Controls.Add(this.webBrowser1);
+            this.panel1.Controls.Add(this.panel2);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1009, 599);
+            this.panel1.Size = new System.Drawing.Size(876, 475);
             this.panel1.TabIndex = 3;
             // 
             // webBrowser1
             // 
             this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.webBrowser1.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser1.Location = new System.Drawing.Point(0, 20);
             this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
             this.webBrowser1.Name = "webBrowser1";
             this.webBrowser1.ScriptErrorsSuppressed = true;
-            this.webBrowser1.Size = new System.Drawing.Size(1009, 599);
+            this.webBrowser1.Size = new System.Drawing.Size(876, 455);
             this.webBrowser1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.ReceivePortNumberBox);
+            this.panel2.Controls.Add(this.PrefixTextBox);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.TargetPortNumberBox);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.TargetIPTextBox);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(876, 20);
+            this.panel2.TabIndex = 1;
+            // 
+            // PrefixTextBox
+            // 
+            this.PrefixTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.PrefixTextBox.Dock = System.Windows.Forms.DockStyle.Left;
+            this.PrefixTextBox.Location = new System.Drawing.Point(272, 0);
+            this.PrefixTextBox.Name = "PrefixTextBox";
+            this.PrefixTextBox.Size = new System.Drawing.Size(100, 13);
+            this.PrefixTextBox.TabIndex = 5;
+            this.PrefixTextBox.Text = "/timeliner";
+            // 
+            // label3
+            // 
+            this.label3.Dock = System.Windows.Forms.DockStyle.Left;
+            this.label3.Location = new System.Drawing.Point(233, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Prefix";
+            // 
+            // TargetPortNumberBox
+            // 
+            this.TargetPortNumberBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.TargetPortNumberBox.Dock = System.Windows.Forms.DockStyle.Left;
+            this.TargetPortNumberBox.Location = new System.Drawing.Point(183, 0);
+            this.TargetPortNumberBox.Maximum = new decimal(new int[] {
+            65535,
+            0,
+            0,
+            0});
+            this.TargetPortNumberBox.Name = "TargetPortNumberBox";
+            this.TargetPortNumberBox.Size = new System.Drawing.Size(50, 16);
+            this.TargetPortNumberBox.TabIndex = 2;
+            this.TargetPortNumberBox.Value = new decimal(new int[] {
+            4444,
+            0,
+            0,
+            0});
+            this.TargetPortNumberBox.ValueChanged += new System.EventHandler(this.NumericUpDown1ValueChanged);
+            // 
+            // label2
+            // 
+            this.label2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.label2.Location = new System.Drawing.Point(152, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Port";
+            // 
+            // TargetIPTextBox
+            // 
+            this.TargetIPTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.TargetIPTextBox.Dock = System.Windows.Forms.DockStyle.Left;
+            this.TargetIPTextBox.Location = new System.Drawing.Point(58, 0);
+            this.TargetIPTextBox.Name = "TargetIPTextBox";
+            this.TargetIPTextBox.Size = new System.Drawing.Size(94, 13);
+            this.TargetIPTextBox.TabIndex = 0;
+            this.TargetIPTextBox.Text = "127.0.0.1";
+            this.TargetIPTextBox.TextChanged += new System.EventHandler(this.TextBox1TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Target IP";
             // 
             // timer1
             // 
             this.timer1.Interval = 1;
             // 
+            // ReceivePortNumberBox
+            // 
+            this.ReceivePortNumberBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.ReceivePortNumberBox.Dock = System.Windows.Forms.DockStyle.Right;
+            this.ReceivePortNumberBox.Location = new System.Drawing.Point(825, 0);
+            this.ReceivePortNumberBox.Maximum = new decimal(new int[] {
+            65535,
+            0,
+            0,
+            0});
+            this.ReceivePortNumberBox.Name = "ReceivePortNumberBox";
+            this.ReceivePortNumberBox.Size = new System.Drawing.Size(51, 16);
+            this.ReceivePortNumberBox.TabIndex = 9;
+            this.ReceivePortNumberBox.Value = new decimal(new int[] {
+            5555,
+            0,
+            0,
+            0});
+            this.ReceivePortNumberBox.ValueChanged += new System.EventHandler(this.ReceivePortNumberBoxValueChanged);
+            // 
+            // label4
+            // 
+            this.label4.Dock = System.Windows.Forms.DockStyle.Right;
+            this.label4.Location = new System.Drawing.Point(753, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(72, 20);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Receive Port";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1009, 599);
+            this.ClientSize = new System.Drawing.Size(876, 475);
             this.Controls.Add(this.panel1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "TimelinerSA";
             this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.TargetPortNumberBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ReceivePortNumberBox)).EndInit();
             this.ResumeLayout(false);
         }
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown ReceivePortNumberBox;
+        private System.Windows.Forms.TextBox PrefixTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.NumericUpDown TargetPortNumberBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TargetIPTextBox;
+        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.WebBrowser webBrowser1;
         private System.Windows.Forms.Panel panel1;
--- TimelinerSA/Form1.cs
+++ TimelinerSA/Form1.cs
@@ -4,90 +4,252 @@ using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Windows.Forms;
-using System.Xml.Linq;
 using System.Diagnostics;
+using System.Net;
+using System.Threading;
 
 using Timeliner;
 using Posh;
+using VVVV.Utils.OSC;
 
 namespace TimeLinerSA
 {
-	public partial class Form1 : Form, IDisposable
-	{
-		private List<PoshTimeliner> FPoshTimeliners = new List<PoshTimeliner>();
-		private WebBrowser FWebBrowser;
-		private Stopwatch Clock = new Stopwatch();
+    public partial class Form1 : Form, IDisposable
+    {
+        List<PoshTimeliner> FPoshTimeliners = new List<PoshTimeliner>();
+        WebBrowser FWebBrowser;
+        Stopwatch Clock = new Stopwatch();
+        OSCTransmitter FOSCTransmitter;
+        OSCReceiver FOSCReceiver;
+        bool FListening = false;
+        Thread FOSCListener;
 		
-		public Form1()
-		{
-			InitializeComponent();
+        public Form1()
+        {
+            InitializeComponent();
 			
-			//register url on http server
-			WebServer.TerminalPath = Path.Combine(Path.GetDirectoryName(Assembly.GetCallingAssembly().Location), @"web");
-			WebServer.UnknownURL += AddUrl;
-			WebServer.OpenURL += OpenUrl;
+            //register url on http server
+            WebServer.TerminalPath = Path.Combine(Path.GetDirectoryName(Assembly.GetCallingAssembly().Location), @"web");
+            WebServer.UnknownURL += AddUrl;
+            WebServer.OpenURL += OpenUrl;
 							
-			webBrowser1.Navigate("about:blank");
-			webBrowser1.Navigate(new Uri("http://localhost:4444/timeliner"));
+            webBrowser1.Navigate("about:blank");
+            webBrowser1.Navigate(new Uri("http://localhost:4444/timeliner"));
+            
+            OpenTransmitter();
+			StartOSCReceiver();
 			
-			timer1.Interval = 1000 / 30;
-			timer1.Tick += timer1_Tick;
-			timer1.Start();
-			//FWAMPTimeliner.SaveData = xml => FData[0] = xml.ToString();
+            timer1.Interval = 1000 / 30;
+            timer1.Tick += timer1_Tick;
+            timer1.Start();
+            //FWAMPTimeliner.SaveData = xml => FData[0] = xml.ToString();
 			
-			Clock.Start();
+            Clock.Start();
 			
-			//dispose web- and wampserver
-			this.Disposed += (s, e) => 
-			{
-				WebServer.Stop();
-				foreach (var tl in FPoshTimeliners) 
-				{
-					tl.Dispose();
-				}
-			};
+            //dispose web- and wampserver
+            this.Disposed += (s, e) => 
+            {
+                WebServer.Stop();
+                CloseTransmitter();
+                
+                foreach (var tl in FPoshTimeliners) 
+                {
+                    tl.Dispose();
+                }
+            };
 			
-		}
+        }
+        		
+        void timer1_Tick(object sender, EventArgs e)
+        {
+            var hosttime = Clock.ElapsedMilliseconds / 1000f;
+            var bundle = new OSCBundle();
+            var prefix = "/" + PrefixTextBox.Text.Trim('/');
+            
+            lock(FPoshTimeliners)
+                foreach (var timeliner in FPoshTimeliners)
+                {
+                    timeliner.Evaluate(hosttime);
+                    foreach (var tl in timeliner.Timeliner.TimelineView.Tracks)
+                    {
+                        var label = (tl as ValueTrackView).Model.Label.Value;
+                        var val = (tl as ValueTrackView).Model.CurrentValue;
+                        
+                        var message = new OSCMessage(prefix + "/" + label);
+                        message.Append((float) val);
+                        bundle.Append(message);                    
+                    }
+                }
+            
+            if (FOSCTransmitter != null)
+                FOSCTransmitter.Send(bundle);
+        }
 		
-		void timer1_Tick(object sender, EventArgs e)
-		{
-			var hosttime = Clock.ElapsedMilliseconds / 1000f;
-        	
-			lock(FPoshTimeliners)
-				foreach (var timeliner in FPoshTimeliners)
-					timeliner.Evaluate(hosttime);
-		}
-		
-		private PoshTimeliner AddTimeliner(string url)
-		{
-			var _url = WebServer.AddURL(url);
-			var port = WebServer.URLPort[_url];
-			PoshTimeliner timeliner;
-			lock(FPoshTimeliners)
-			{
-				FPoshTimeliners.Add(new PoshTimeliner(_url, port));
-				timeliner = FPoshTimeliners.Last(); 
-			}
+        private PoshTimeliner AddTimeliner(string url)
+        {
+            var _url = WebServer.AddURL(url);
+            var port = WebServer.URLPort[_url];
+            PoshTimeliner timeliner;
+            lock(FPoshTimeliners)
+            {
+                FPoshTimeliners.Add(new PoshTimeliner(_url, port));
+                timeliner = FPoshTimeliners.Last(); 
+            }
 			
-			timeliner.Log = x => Console.WriteLine(x);
-			return timeliner;
-		}
+            timeliner.Log = x => Console.WriteLine(x);
+            return timeliner;
+        }
         
-		private void AddUrl(string url)
-		{
-			if (!url.Contains("."))
-				AddTimeliner(url);
-		}
+        private void AddUrl(string url)
+        {
+            if (!url.Contains("."))
+                AddTimeliner(url);
+        }
         
-		private void OpenUrl(string url)
-		{
-			if (!WebServer.URLPort.ContainsKey(url))
-			{
-				var timeliner = AddTimeliner(url);
-				var path = Path.Combine(WebServer.TerminalPath, url) + ".xml";
+        private void OpenUrl(string url)
+        {
+            if (!WebServer.URLPort.ContainsKey(url))
+            {
+                var timeliner = AddTimeliner(url);
+                var path = Path.Combine(WebServer.TerminalPath, url) + ".xml";
 				var element = XElement.Load(path);
 				timeliner.LoadData(element);
+            }
+        }
+        
+        void NumericUpDown1ValueChanged(object sender, System.EventArgs e)
+        {
+            UpdateTransmitter();
+        }
+        
+        void TextBox1TextChanged(object sender, System.EventArgs e)
+        {
+            UpdateTransmitter();
+        }
+        
+        void ReceivePortNumberBoxValueChanged(object sender, System.EventArgs e)
+		{
+            UpdateReceiver();
+		}
+        
+        #region OSC
+        void UpdateTransmitter()
+        {
+            CloseTransmitter();
+            OpenTransmitter();
+        }
+        
+        void CloseTransmitter()
+        {
+            if (FOSCTransmitter != null)
+            {
+                FOSCTransmitter.Close();
+                FOSCTransmitter = null;
+            }
+        }
+        
+        void OpenTransmitter()
+        {
+            var ip = TargetIPTextBox.Text.Trim();
+            try
+            {
+                var ipAddress = IPAddress.Parse(TargetIPTextBox.Text.Trim()); 
+                
+                FOSCTransmitter = new OSCTransmitter(ip, (int) TargetPortNumberBox.Value);
+                FOSCTransmitter.Connect();
+            }
+            catch
+            {}
+        }
+        
+        void UpdateReceiver()
+        {
+            StopOSCReceiver();
+            StartOSCReceiver();
+        }
+        
+		void StartOSCReceiver()
+		{
+            FOSCReceiver = new OSCReceiver((int) ReceivePortNumberBox.Value);
+			FOSCListener = new Thread(new ThreadStart(ListenToOSC));
+			
+            FListening = true;
+            FOSCListener.Start();
+		}
+		
+		void StopOSCReceiver()
+		{
+			FListening = false;
+            FOSCListener.Abort();
+			if (FOSCReceiver != null)
+				FOSCReceiver.Close();
+
+			FOSCReceiver = null;
+		}
+
+		void ListenToOSC()
+		{
+			while(FListening)
+			{
+				try
+				{
+					var packet = FOSCReceiver.Receive();
+					if (packet!=null)
+					{
+						if (packet.IsBundle())
+						{
+							var messages = packet.Values;
+							for (int i=0; i<messages.Count; i++)
+								ProcessOSCMessage((OSCMessage)messages[i]);
+						}
+						else
+							ProcessOSCMessage((OSCMessage)packet);
+					}
+				}
+				catch (Exception e)
+				{
+				}
 			}
 		}
-	}
+		
+		void ProcessOSCMessage(OSCMessage message)
+		{
+			var address = message.Address;
+			var args = message.Values;
+			
+			char[] s = {'/'};
+			string[] path = address.Split(s);
+			
+            if (path[1] == PrefixTextBox.Text.Trim('/'))
+                switch(path[2])
+            {
+                case "play":
+                    {
+                        var arg = (int) args[0];
+                        FPoshTimeliners[0].Timeliner.Timer.Play(arg == 1);
+                        break;
+                    }
+                case "stop":
+                    {
+                        FPoshTimeliners[0].Timeliner.Timer.Stop();
+                        break;
+                    }
+                case "seek":
+                    {
+                        FPoshTimeliners[0].Timeliner.Timer.Time = (float) args[0];
+                        break;
+                    }
+                case  "loop":
+                    {
+                        FPoshTimeliners[0].Timeliner.Timeline.Ruler.LoopStart.Value = (float) args[0];
+                        FPoshTimeliners[0].Timeliner.Timeline.Ruler.LoopEnd.Value = (float) args[1];
+                        FPoshTimeliners[0].Timeliner.TimelineView.UpdateScene();
+                        break;
+                    }
+            }
+		}
+		#endregion OSC
+            }
+		}
+    }
 }
